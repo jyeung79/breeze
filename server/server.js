9d49928345e6ffc38fa52457cee9613641ef0bdd
zjyeung <zjyeung579@gmail.com>
2020-04-28 23:12:53 -0700

    Set up routes

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const cors = require('cors');
 
 const app = express();
 const helper = require('./helper');
-const api = process.env.DARK_SKY_API_KEY;
-let port = process.env.PORT || 8000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
@@ -18,18 +16,6 @@ require('./routes')(app);
 app.get('/', function (req, res) {
     res.send('PORT 3000');
 })
-
-app.post('/', async function (req, res) {
-    try {
-        let urlDark = `https://api.darksky.net/forecast/${api}/${lat},${lng}?exclude=minutely,alerts,flags&units=ca`;
-        let weather = await axios.get(urlDark).then(response => {return response.data});
-        //console.log(weather);
-        res.send()
-    } catch (err) {
-        res.render('index', {weatherObject: null, error: err, format: null, location: null});
-    }
-})
-
 
 app.listen(port, (err) => {
     if (err) { console.log(err) };
