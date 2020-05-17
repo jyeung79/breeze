d66f393c0878946fb03e3cc0785ea8d4ab8314b5
zjyeung <zjyeung579@gmail.com>
2020-05-16 17:42:45 -0700

    Server returns proper weather data

--- server/server.js
+++ server/server.js
@@ -1,21 +1,39 @@
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
 
 const app = express();
-//const helper = require('./helper');
 const port = process.env.PORT || 5000;
 //const router = express.Router();
-const logger = require('./middleware/logger');
 
+let api = process.env.REACT_APP_DARK_SKY_API;
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-//app.use(cors());
-app.use('/api/members', require('./routes/api/Members'));
 
-app.use(logger);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
 
-app.listen(port, (err) => {
-    if (err) { console.log(err) };
-    console.log('Listening on port ' + port);
+app.get('/search', async function(req, res) {
+    console.log(req.query);
+    if (req.query.lat !== undefined && req.query.lng !== undefined) {
+        let lat = req.query.lat;
+        let lng = req.query.lng;
+        console.log(lat, lng);
+        let url = `https://api.darksky.net/forecast/${api}/${lat},${lng}?exclude=minutely,alerts,flags&units=ca`;
+        
+        try {
+            const weather = await axios.get(url).then(res => {return res.data});
+            console.log(weather);
+            res.send(weather);
+        } catch (err) {
+            res.send(err);
+        }
+    } else {
+        res.send('hi');
+    }
 });
