31fdcf365d80530121a2f3b26d0a04b7c911e3a9
Jeff <zjyeung579@gmail.com>
2020-09-01 19:48:26 -0700

    Removed env and changed api key

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-let api = process.env.REACT_APP_DARK_SKY_API;
+let api = process.env.DARK_SKY_API;
 
 app.use(cors());
 app.use(bodyParser.json());
